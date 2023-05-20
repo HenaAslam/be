2293890665da7859385f9427cefbca8d23aa9ae7
ajeer333 <ajeer333@gmail.com>
2023-05-20 15:56:29 +0200

    Update index.js

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,11 +1,32 @@
-let onlineUsers = [];
+import boardsModel from "../api/boards/model.js";
+let onlineUsers = new Map();
 
 export const newConnectionHandler = (socket) => {
-  console.log("A new client connected! Id:", socket.id);
-
-  socket.emit("welcome", { message: `HELLO ${socket.id}` });
-
-  socket.on("disconnect", () => {
-    console.log("A client disconnected. Id:", socket.id);
-  });
+  // console.log("A new client connected! Id:", socket.id);
+  // socket.on("sendUserId", (userId) => {
+  //   onlineUsers.set(userId, socket);
+  //   console.log(onlineUsers);
+  // });
+  // socket.emit("welcome", { message: `HELLO ${socket.id}` });
+  // socket.on("onDrag", async ({ _id, columns }) => {
+  //   const board = await boardsModel.findById(_id);
+  //   if (board.members.length > 0) {
+  //     board.members.forEach((member) => {
+  //       const targetMember = onlineUsers.get(member._id.toString());
+  //       if (targetMember) {
+  //         targetMember.emit("updateDraggedColumns", { _id, columns });
+  //       }
+  //     });
+  //   }
+  // });
+  // socket.on("disconnect", () => {
+  //   console.log("A client disconnected. Id:", socket.id);
+  //   onlineUsers.forEach((value, key, map) => {
+  //     if (value.id === socket.id) {
+  //       map.delete(key);
+  //       return;
+  //     }
+  //   });
+  //   console.log(onlineUsers);
+  // });
 };
